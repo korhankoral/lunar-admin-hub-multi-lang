4b87eb38d6d6daf2000e07fc089f83365cf45279
Korhan Koral <korhanmonotech@gmail.com>
2023-11-26 16:56:20 +0300

    Added Service Provider

---
+++ src/AdminHubMultiLangServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Korhankoral\LunarAdminHubMultiLang;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminHubServiceProvider extends ServiceProvider
+{
+
+    protected $root = __DIR__.'/..';
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot up the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'adminhub');
+
+        $this->configurePublishing();
+    }
+
+    /**
+     * Configure publishing for the package.
+     *
+     * @return void
+     */
+    protected function configurePublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../resources/lang' => lang_path('vendor/adminhub'),
+            ], 'lunar.hub.translations');
+        }
+    }
+}
